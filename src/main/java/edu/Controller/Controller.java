897e0f713ebf2242e89975db04103f08a8ce0eed
Thxrunn <tharundina@gmail.com>
2026-04-12 12:04:52 -0700

    Implement login and registration functionality
    Added login and registration methods to handle user authentication and athlete registration.

--- src/main/java/edu/Controller/Controller.java
+++ src/main/java/edu/Controller/Controller.java
@@ -2,14 +2,30 @@ package edu.Controller;
 
 //File Name: Controller.java
 //Group: 3
-//Date: 3/21/2026
+//Date: 3/37/2026
 //Description: This class handles connections between UI and database(SQLite)
 
 public class Controller {
 
+    private SQliteLoginManager loginManager;
+    private SQliteRegistrationManager registrationManager;
     private SQLiteJoinEventManager joinEventManager;
+
     public Controller() {
+        loginManager = new SQliteLoginManager();
+        registrationManager = new SQliteRegistrationManager();
         joinEventManager = new SQLiteJoinEventManager();
+    }
+
+    public int login(String username, String password) {
+        if (loginManager.authenticateUser(username, password)) {
+            return loginManager.getAthleteId(username, password);
+        }
+        return -1;
+    }
+
+    public boolean register(String username, String password) {
+        return registrationManager.registerAthlete(username, password);
     }
 
     public String joinEvent(int athleteId, int eventId) {
@@ -36,5 +52,13 @@ public class Controller {
         } else {
             return "JOIN_FAILED";
         }
+    }
+
+    public Object[][] getAllEvents() {
+        return joinEventManager.getAllEvents();
+    }
+
+    public boolean createEvent(String eventName, String sport, String eventDate, String location, String description, int maxPlayers) {
+        return joinEventManager.createEvent(eventName, sport, eventDate, location, description, maxPlayers);
     }
 }
