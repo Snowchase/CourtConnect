b229325d6491eda82cdeb8c5f242207d23059581
Thxrunn <tharundina@gmail.com>
2026-04-13 20:13:04 -0700

    Update SQliteLoginManager.java

--- src/main/java/edu/Controller/SQliteLoginManager.java
+++ src/main/java/edu/Controller/SQliteLoginManager.java
@@ -2,10 +2,6 @@ package edu.Controller;
 
 import java.sql.*;
 
-//File Name: SQliteLoginManager.java
-//Group: 3
-//Date: 3/37/2026
-//Description: This class manages all data requests to SQlite regarding login and returns data to the controller
 public class SQliteLoginManager {
 
     private static final String DB_URL = "jdbc:sqlite:database.db";
@@ -16,6 +12,23 @@ public class SQliteLoginManager {
     public boolean authenticateUser(String user, String pass) {
         try {
             Connection conn = DriverManager.getConnection(DB_URL);
+
+            System.out.println("LOGIN CHECK DB_URL = " + DB_URL);
+            System.out.println("Trying login for username = " + user);
+
+            Statement stmt = conn.createStatement();
+            ResultSet allUsers = stmt.executeQuery("SELECT athlete_id, username, password FROM athletes");
+
+            System.out.println("Current athletes in DB:");
+            while (allUsers.next()) {
+                System.out.println(
+                        "athlete_id=" + allUsers.getInt("athlete_id") +
+                        ", username=" + allUsers.getString("username") +
+                        ", password=" + allUsers.getString("password")
+                );
+            }
+            allUsers.close();
+            stmt.close();
 
             PreparedStatement pstmt = conn.prepareStatement(
                     "SELECT 1 FROM athletes WHERE username = ? AND password = ?"
@@ -30,12 +43,7 @@ public class SQliteLoginManager {
             pstmt.close();
             conn.close();
 
-            if (authenticated) {
-                System.out.println("User authenticated");
-            } else {
-                System.out.println("User not found");
-            }
-
+            System.out.println("Authenticated = " + authenticated);
             return authenticated;
 
         } catch (SQLException e) {
@@ -65,6 +73,7 @@ public class SQliteLoginManager {
             pstmt.close();
             conn.close();
 
+            System.out.println("Resolved athleteId = " + athleteId);
             return athleteId;
 
         } catch (SQLException e) {
